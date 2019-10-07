f6bda479619bbb76072c7abc4c80cbd7abadaa30
Abhinav Sharma <abhi18av@gmail.com>
2019-10-07 17:49:44 +0530

    chunk the src files into client-ui-app-api

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -1,160 +1,22 @@
 (ns app.client
   (:require
-    ;; project libs
-    [app.secrets :as secrets]
-    [app.utils :as utils :refer [clog]]
-    [app.pathom :as p :refer [pathom-api]]
-    [app.temp-db :refer [temp-db]]
 
-    ;; external libs
-    [clojure.string :as str]
+    ;; project libs
+    [app.application :refer [APP]]
+    [app.ui :as ui]
+    [app.utils :as utils :refer [clog]]
 
     ;; internal libs
-    [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
-    [com.fulcrologic.fulcro.algorithms.merge :as merge]
-    [com.fulcrologic.fulcro.mutations :as m]))
-
-;; TODO add workspaces to the project
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; COMPONENTS
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+    [com.fulcrologic.fulcro.application :as app]))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; FirstComponent Component
+;; APP init
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(def temp-db-data (atom {}))
-
-(m/defmutation update-temp-db [_]
-  (action [{:keys [state]}]
-          (clog {:message "[FirstComponent] MUTATION update-temp-db" :color "magenta" :props state})
-          (reset! temp-db-data temp-db)))
-
-(comment
-  (comp/transact! APP [(update-temp-db)])
-  @temp-db-data
-
-  (app/schedule-render! APP)
-  )
-
-(defsc FirstComponent [this props]
-  {#_#_:query []
-   #_#_:ident []
-   :initial-state      {}
-   :initLocalState     (fn [this]
-                         (clog {:message "[FirstComponent]: InitLocalState" :color "teal"}))
-   :componentDidMount  (fn [this]
-                         (let [p (comp/props this)]
-                           (clog {:message "[FirstComponent] MOUNTED" :props p :color "green"})))
-   :componentDidUpdate (fn [this]
-                         (let [p (comp/props this)]
-                           (clog {:message "[FirstComponent]: UPDATED" :color "blue" :props p})))}
-  (comp/fragment
-    (dom/div)))
-
-
-(def ui-first-component (comp/factory FirstComponent))
-
-
-(comment
-
-  (comp/transact! APP [(update-temp-db)])
-
-  @temp-db-data
-
-  (reset! temp-db-data {})
-
-  (reset! (::app/state-atom APP) @temp-db-data)
-
-  (reset! (::app/state-atom APP) {})
-
-  (app/current-state APP)
-
-  (reset! (::app/state-atom APP) {:one 1})
-
-  (app/schedule-render! APP)
-
-  )
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ROOT Component
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defsc Root [this props]
-  {#_#_:query []
-   #_#_:ident []
-
-   :initial-state      {}
-   :initLocalState     (fn [this]
-                         (clog {:message "[Root]: InitLocalState" :color "teal"}))
-   :componentDidMount  (fn [this]
-                         (let [p (comp/props this)]
-                           (clog {:message "[Root] MOUNTED (with TimeStamp)" :props (js/Date.) :color "green"})))
-   :componentDidUpdate (fn [this]
-                         (let [p (comp/props this)]
-                           (clog {:message "[Root]: UPDATED" :color "blue" :props p})))}
-  (comp/fragment
-    (dom/h1 :.ui.header "Hello, Fulcro!")
-    #_(ui-first-component temp-db)))
-
-
-(comment
-
-  (comp/transact! APP [(update-temp-db)])
-
-  @temp-db-data
-
-  (reset! temp-db-data {})
-
-  (reset! (::app/state-atom APP) @temp-db-data)
-
-  (reset! (::app/state-atom APP) {})
-
-  (app/current-state APP)
-
-  (reset! (::app/state-atom APP) {:one 1})
-
-  (app/schedule-render! APP)
-
-  )
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; APP definition and init
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defonce APP (app/fulcro-app))
 
 (defn ^:export init []
-  (app/mount! APP Root "app"))
+  (app/mount! APP ui/Root "app")
+  (clog {:message "[APP]: INITIALIZED" :color "orange"}))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(comment
-  (shadow/repl :main)
-
-  ;; js hard reset
-  (.reload js/location true)
-
-  (-> APP
-      (::app/state-atom)
-      deref)
-
-  (app/schedule-render! APP {:force-root? true})
-
-  (reset! (::app/state-atom APP) {})
-
-  (app/current-state APP)
-
-  )
 
--- src/app/pathom.cljs
+++ src/app/pathom.cljs
@@ -10,153 +10,27 @@
             [com.wsscode.pathom.diplomat.http :as p.http]))
 
 
-(defn adapt-core [core]
-  (-> core
-      (namespaced-keys "spacex.core")))
+;;=============== utils ================
 
-(defn adapt-payload [payload]
-  (-> payload
-      (namespaced-keys "spacex.payload")
-      (pull-namespaced :spacex.payload/orbit-params "spacex.payload.orbit-params")))
+(declare pathom-api)
 
-(defn adapt-rocket [rocket]
-  (-> rocket
-      (namespaced-keys "spacex.rocket")
-      (pull-namespaced :spacex.rocket/first-stage "spacex.launch.first-stage")
-      (update-if :spacex.launch.first-stage/cores #(mapv adapt-core %))
-      (pull-namespaced :spacex.rocket/second-stage "spacex.launch.second-stage")
-      (update-if :spacex.launch.second-stage/payloads #(mapv adapt-payload %))
-      (pull-namespaced :spacex.rocket/fairings "spacex.launch.fairings")))
+;;=============== api adapters ================
 
-(defn adapt-launch-pad [launch-pad]
-  (-> launch-pad
-      (namespaced-keys "spacex.launch-pad")))
 
-(defn adapt-launch [launch]
-  (-> launch
-      (update :launch_site adapt-launch-pad)
-      (update :rocket adapt-rocket)
-      (namespaced-keys "spacex.launch")
-      (pull-key :spacex.launch/launch-site)
-      (pull-key :spacex.launch/rocket)
-      (pull-namespaced :spacex.launch/links "spacex.launch.links")
-      (pull-namespaced :spacex.launch/telemetry "spacex.launch.telemetry")))
+;;=============== api shape ================
 
-(def launch-out
-  [:spacex.launch/details
-   :spacex.launch/flight-number
-   :spacex.launch/is-tentative
-   :spacex.launch/launch-date-local
-   :spacex.launch/launch-date-unix
-   :spacex.launch/launch-date-utc
-   :spacex.launch/launch-success
-   :spacex.launch/launch-year
-   :spacex.launch/mission-id
-   :spacex.launch/mission-name
-   :spacex.launch/ships
-   :spacex.launch/static-fire-date-unix
-   :spacex.launch/static-fire-date-utc
-   :spacex.launch/tentative-max-precision
-   :spacex.launch/upcoming
-   :spacex.launch-pad/site-id
-   :spacex.launch-pad/site-name
-   :spacex.launch-pad/site-name-long
-   :spacex.launch.fairings/recovered
-   :spacex.launch.fairings/recovery-attempt
-   :spacex.launch.fairings/reused
-   :spacex.launch.fairings/ship
-   {:spacex.launch.first-stage/cores [:spacex.core/block
-                                      :spacex.core/core-serial
-                                      :spacex.core/flight
-                                      :spacex.core/gridfins
-                                      :spacex.core/land-success
-                                      :spacex.core/landing-intent
-                                      :spacex.core/landing-type
-                                      :spacex.core/landing-vehicle
-                                      :spacex.core/legs
-                                      :spacex.core/reused]}
-   :spacex.launch.links/article-link
-   :spacex.launch.links/flickr-images
-   :spacex.launch.links/mission-patch
-   :spacex.launch.links/mission-patch-small
-   :spacex.launch.links/video-link
-   :spacex.launch.links/wikipedia
-   :spacex.launch.second-stage/block
-   {:spacex.launch.second-stage/payloads [:spacex.payload/customers
-                                          :spacex.payload/manufacturer
-                                          :spacex.payload/nationality
-                                          :spacex.payload/norad-id
-                                          :spacex.payload/orbit
-                                          :spacex.payload/payload-id
-                                          :spacex.payload/payload-mass-kg
-                                          :spacex.payload/payload-mass-lbs
-                                          :spacex.payload/payload-type
-                                          :spacex.payload/reused
-                                          :spacex.payload.orbit-params/apoapsis-km
-                                          :spacex.payload.orbit-params/arg-of-pericenter
-                                          :spacex.payload.orbit-params/eccentricity
-                                          :spacex.payload.orbit-params/epoch
-                                          :spacex.payload.orbit-params/inclination-deg
-                                          :spacex.payload.orbit-params/lifespan-years
-                                          :spacex.payload.orbit-params/longitude
-                                          :spacex.payload.orbit-params/mean-anomaly
-                                          :spacex.payload.orbit-params/mean-motion
-                                          :spacex.payload.orbit-params/periapsis-km
-                                          :spacex.payload.orbit-params/period-min
-                                          :spacex.payload.orbit-params/raan
-                                          :spacex.payload.orbit-params/reference-system
-                                          :spacex.payload.orbit-params/regime
-                                          :spacex.payload.orbit-params/semi-major-axis-km :spacex.payload/cap-serial
-                                          :spacex.payload/cargo-manifest
-                                          :spacex.payload/flight-time-sec
-                                          :spacex.payload/mass-returned-kg
-                                          :spacex.payload/mass-returned-lbs]}
-   :spacex.launch.telemetry/flight-club
-   :spacex.rocket/rocket-id
-   :spacex.rocket/rocket-name
-   :spacex.rocket/rocket-type
-   :spacex.launch.links/presskit
-   :spacex.launch.links/reddit-campaign
-   :spacex.launch.links/reddit-launch
-   :spacex.launch.links/reddit-media
-   :spacex.launch.links/reddit-recovery])
+
+;;=============== api resolvers ================
+
+
+;;=============== api resolvers ================
 
 
 
-(pc/defresolver latest-launch
-  [env _]
-  {::pc/output [{:spacex/latest-launch launch-out}]}
-  (go-catch
-    (->> (p.http/request env "https://api.spacexdata.com/v3/launches/latest"
-                         {::p.http/accept ::p.http/json}) <?maybe
-         ::p.http/body
-         adapt-launch
-         (hash-map :spacex/latest-launch))))
 
-
-(comment
-
-  (spacex-api {} [:spacex/latest-launch])
-
-  (spacex-api {} [{:spacex/latest-launch [:spacex/all-launches]}])
-
-  (spacex-api {} [{:spacex/latest-launch [:spacex.launch/mission-name]}])
-
-  )
-
-
-(def resolvers
-  [latest-launch])
-
-(defn spacex-plugin []
-  {::pc/register resolvers})
-
+;;=============== api parser ================
 
 (def http-driver p.http.fetch/request-async)
-
-
-(def app-registry
-  [(pc/alias-resolver :conj-pathom.favorite-launch/flight-number :spacex.launch/flight-number)])
 
 
 (defonce indexes (atom {}))
@@ -169,16 +43,17 @@
                                             p/env-placeholder-reader]
                   ::p/placeholder-prefixes #{">"}
                   ::p.http/driver          http-driver}
-     ::p/plugins [(pc/connect-plugin {#_#_::pc/register app-registry
+     ::p/plugins [(pc/connect-plugin {;;::pc/register app-registry
                                       ::pc/indexes indexes})
-                  (spacex-plugin)
+                  ;; plugin
                   p/error-handler-plugin
                   p/trace-plugin]}))
 
 
 
+;;=============== api function ================
 
-(defn spacex-api [entity query]
+(defn pathom-api [entity query]
   (take! (parser {::p/entity (atom entity)} query) prn))
 
 
